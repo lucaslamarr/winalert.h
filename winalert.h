31319be37e1a513c8b1a274195bd1ad8a25f9439
Lucas LaMarr <59452468+lucaslamarr@users.noreply.github.com>
2024-02-10 14:15:34 -0500

    Core files added.
    Added the following files.
    
    - winalert.c 
    - winalert.h
    - user demo

---
+++ winalert.h
@@ -0,0 +1,17 @@
+#ifndef WINLOG_H
+#define WINLOG_H
+
+/* Choose between three types of messages that will be used for showing severity in debug messages. */
+typedef enum
+{
+	MESSAGE, 
+	WARNING,
+	ERR     /* ERROR keyword not allowed so ERR is used instead. */
+} MessageType;
+
+/* Show a graphical popup. */
+void show_alert(MessageType, const char*);
+
+#endif
+
+
